f08d607740136936b8df00efce33b715e7e4fa18
Alex Rowley <alex.rowley@gmail.com>
2025-01-12 17:05:06 +0000

    add mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "ultrasonic.h"
 #include "ir.h"
 #include "message_queue.h"
+#include "mqtt.h"
 #include "car-board-version.h"
 
 void v_blink_task(void*){
@@ -36,13 +37,15 @@ int main(){
     auto mq = message_queue_init();
 
 
-    xTaskCreate(v_blink_task,"LED",256, nullptr,10, nullptr);
+    //xTaskCreate(v_blink_task,"LED",256, nullptr,10, nullptr);
+    xTaskCreate(v_mqtt_task,"MQTT",256, mq,9, nullptr);
     xTaskCreate(v_lcd_task,"LCD",128, nullptr,9, nullptr);
     xTaskCreate(v_servo_task,"SERVO",128, nullptr,8, nullptr);
     xTaskCreate(v_ws2812_task,"WS2812",128, nullptr,7, nullptr);
     xTaskCreate(v_motor_task,"MOTOR",128, nullptr,6, nullptr);
     xTaskCreate(v_ultrasonic_task,"DST",128, nullptr,5, nullptr);
     xTaskCreate(v_ir_task,"IR",128, nullptr,1, nullptr);
+
 
     vTaskStartScheduler();
     return 0;
